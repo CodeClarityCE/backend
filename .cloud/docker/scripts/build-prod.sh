4a5943095b2baaddb0d48ff461074a45e18be0e6
CedricHerzog <CedricHerzog@users.noreply.github.com>
2024-10-29 13:10:08 +0100

    Adapt copy of .netrc as they might not be present if all go repository are public

--- .cloud/docker/scripts/build-prod.sh
+++ .cloud/docker/scripts/build-prod.sh
@@ -1,13 +1,13 @@
 for f in ./plugins/* ; do
 	cd $f
-	cp ../../.cloud/docker/config/.netrc .cloud/docker/config/.netrc
+	cp -r ../../.cloud/docker/config .cloud/docker/config
 	make build-prod
 	cd -
 done
 
 for f in ./services/* ; do
 	cd $f
-	cp ../../.cloud/docker/config/.netrc .cloud/docker/config/.netrc
+	cp -r ../../.cloud/docker/config .cloud/docker/config
 	make build-prod
 	cd -
 done
